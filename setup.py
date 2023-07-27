82d198d4a9502c14a263db4116f0d46d92a5f211
Pareekshith Katti <ambee@Pareekshiths-MacBook-Air.local>
2023-07-27 14:41:23 +0530

    changed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-long_description = open(os.path.join(here, 'README.md')).read()
+long_description = open(os.path.join(here, "README.md")).read()
 
 setup(
     name="gspatial_tools",
@@ -11,19 +11,26 @@ setup(
     version="0.1.0a",
     author="Ambee",
     license="MIT",
-    url='https://github.com/ambeelabs/gspatial_tools/tree/main/gspatial_tools',
+    url="https://github.com/ambeelabs/gspatial_tools/",
     description="Set of utility tools built on top of Geopandas, Xarray, Rasterio and Rioxarray",
     long_description_content_type="text/markdown",
     long_description=long_description,
     install_requires=[
+        "geopandas>=0.13.2",
+        "numpy>=1.23.5",
+        "pandas>=1.5.3",
+        "rasterio>=1.3.8",
+        "rioxarray>=0.13.3",
+        "scikit_learn>=1.3.0",
+        "shapely>=2.0.1",
     ],
-     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.10',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
     ],
-    keywords='geospatial utilities geopandas rasterio rioxarray xarray',
+    keywords="geospatial utilities geopandas rasterio rioxarray xarray",
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     test_suite="tests",
